30166f4583b656d85a72f550fc9c8b578c12a871
Ganang Rizki Saputro <ganangsaputro38@gmail.com>
2022-08-09 18:57:46 +0700

    Update menuutama.java
    intent menuutama ke halaman awal

--- app/src/main/java/com/example/pemainankodedanhuruf/menuutama.java
+++ app/src/main/java/com/example/pemainankodedanhuruf/menuutama.java
@@ -1,14 +1,28 @@
 package com.example.pemainankodedanhuruf;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class menuutama extends AppCompatActivity {
+    Button btn_keluar;
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuutama);
+        btn_keluar= findViewById(R.id.btn_keluar);
+        btn_keluar.setOnClickListener(view -> {
+            if (view.getId() == R.id.btn_keluar) {
+                Intent btnkeluar = new Intent(menuutama.this, MainActivity.class);
+                startActivity(btnkeluar);
+            }
+            Toast.makeText(menuutama.this, "Anda telah keluar!", Toast.LENGTH_SHORT).show();
+        });
     }
 }
