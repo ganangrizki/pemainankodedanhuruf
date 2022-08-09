e53f12d59f18687ea153821e71ae7bd9949b9c99
Ganang Rizki Saputro <ganangsaputro38@gmail.com>
2022-08-09 18:33:49 +0700

    Mengcoding dijava
    Membuat pindah halaman 1 ke halaman menuutama.
     button mulai ke halaman menuutama.

--- app/src/main/java/com/example/pemainankodedanhuruf/MainActivity.java
+++ app/src/main/java/com/example/pemainankodedanhuruf/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.pemainankodedanhuruf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_mulai;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_mulai=(Button) findViewById(R.id.btn_mulai);
+        btn_mulai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.btn_mulai:
+                        Intent menuutama= new Intent(MainActivity.this, com.example.pemainankodedanhuruf.menuutama.class);
+                        startActivity(menuutama);
+                        break;
+                }
+
+            }
+        });
     }
 }
