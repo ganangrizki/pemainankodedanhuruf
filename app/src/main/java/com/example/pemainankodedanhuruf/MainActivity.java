fc3cd75d12f6ac1b6f6ed7ec1e450ba683f747c1
Ganang Rizki Saputro <ganangsaputro38@gmail.com>
2022-08-09 18:44:12 +0700

    Intent
    halaman menu mulai ke menu utama

--- app/src/main/java/com/example/pemainankodedanhuruf/MainActivity.java
+++ app/src/main/java/com/example/pemainankodedanhuruf/MainActivity.java
@@ -2,31 +2,27 @@ package com.example.pemainankodedanhuruf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_mulai;
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_mulai=(Button) findViewById(R.id.btn_mulai);
-        btn_mulai.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                switch (view.getId()){
-                    case R.id.btn_mulai:
-                        Intent menuutama= new Intent(MainActivity.this, com.example.pemainankodedanhuruf.menuutama.class);
-                        startActivity(menuutama);
-                        break;
-                }
-                Toast.makeText(MainActivity.this, "Anda berhasil ke menu utama", Toast.LENGTH_SHORT).show();
-
+        btn_mulai= findViewById(R.id.btn_mulai);
+        btn_mulai.setOnClickListener(view -> {
+            if (view.getId() == R.id.btn_mulai) {
+                Intent menuutama = new Intent(MainActivity.this, com.example.pemainankodedanhuruf.menuutama.class);
+                startActivity(menuutama);
             }
+            Toast.makeText(MainActivity.this, "Anda berhasil ke menu utama", Toast.LENGTH_SHORT).show();
+
         });
     }
 }
